4232e82d12dcb0b21837bba32066c64f3d11cf4b
DriveCreativeServices <128182273+DriveCreativeServices@users.noreply.github.com>
2023-04-17 14:21:03 -0400

    Update mysql.php

--- includes/mysql.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$user = 'root';
-$pwd = $MARIADB_PASSWORD;
-$db = 'crgmich';
-
-$con = new mysqli('node12664-crg-prod.us.reclaim.cloud', $user, $pwd, $db) or die("Unable to connect to database");
-
-?>
